72adfaf8e37959d7498b7039cfb2fc5f0b3a6c27
xkaper001 <imluckyayan@gmail.com>
2026-06-04 17:04:36 +0530

    chore: update configuration files and add wrangler.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
